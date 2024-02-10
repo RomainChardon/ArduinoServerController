c3e5490ab1b95d18b726d07b125ddab2b55cc140
RomainChardon <chardon.romain26@gmail.com>
2024-02-10 01:49:03 +0100

    add lcd

--- showPrint/showPrint.ino
+++ showPrint/showPrint.ino
@@ -1,9 +1,18 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);
 String value;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.setTimeout(1);
+
+   lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
 }
 
 void loop() {
