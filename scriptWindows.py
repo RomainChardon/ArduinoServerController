7cfd64c7b7c5a7e3c76d85c56cc07905c16a504a
RomainChardon <chardon.romain26@gmail.com>
2024-02-10 03:02:30 +0100

    add psutil

--- scriptWindows.py
+++ scriptWindows.py
@@ -1,28 +1,30 @@
 import serial
 import time
+import psutil
 
 arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
 
 def write_read(x): 
-    
     # send
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05) 
-    
+
     # return
     data = arduino.readline() 
-    
+
     return data 
 
 while True: 
-    
-    # set data input
-    data = input("Enter a la data: ") 
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory_usage = psutil.virtual_memory()
+    data = f"CPU: {cpu_percent}% - RAM: {memory_usage.percent}"
     
     # call arduino
     value = write_read(data) 
     
     # show result
     print(value)
+    
+    time.sleep(1)
     
 
